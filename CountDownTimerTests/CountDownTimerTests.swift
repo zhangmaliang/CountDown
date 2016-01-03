4e89865a8eb28c28b2829dde4249d9fdeb52e54f
apple <apple@appletekimacbook-pro-2.local>
2016-01-03 10:41:50 +0800

    Initial Commit

---
+++ CountDownTimerTests/CountDownTimerTests.swift
@@ -0,0 +1,36 @@
+//
+//  CountDownTimerTests.swift
+//  CountDownTimerTests
+//
+//  Created by apple on 16/1/3.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+import XCTest
+@testable import CountDownTimer
+
+class CountDownTimerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
